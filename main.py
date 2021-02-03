ed6e4657512c24d715d484f3ffc47d6904fa21cf
Laptop <henmtol@gmail.com>
2021-02-03 00:30:42 +0000

    Split draw and create board function, refactor and improve code efficiency

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
 import pygame
 pygame.init()  # Start Pygame
 
-screen = pygame.display.set_mode((640, 480))  # Start the screen
-sprites = []
-GREY = (50, 50, 50)
 
-
-def draw(width=3, height=None):
+def board_create(width=3, height=None):
     if height is None:
         height = width
 
@@ -15,12 +11,30 @@ def draw(width=3, height=None):
     boarder_x = (screen.get_width() - (width * (box_size + gap) - gap))/2
     boarder_y = (screen.get_height() - (height * (box_size + gap) - gap))/2
 
-    for i in range(width):
-        for j in range(height):
-            pygame.draw.rect(screen, GREY, (boarder_x + (box_size + gap)*i,
-                                            boarder_y + (box_size + gap)*j,
-                                            box_size, box_size))
+    return [pygame.Rect(boarder_x + (box_size + gap) * i,
+                        boarder_y + (box_size + gap) * j,
+                        box_size, box_size) for i in range(width) for j in range(height)]
+    # for i in range(width):
+    #     for j in range(height):
+    #         sprites.append(pygame.Rect(boarder_x + (box_size + gap) * i,
+    #                                    boarder_y + (box_size + gap) * j,
+    #                                    box_size, box_size))
 
+
+def draw(clicked):
+    for i in sprites:
+        pygame.draw.rect(screen, LIGHT_GREY, i)
+    for j in clicked:
+        pygame.draw.rect(screen, (255, 0, 0), j)
+
+
+screen = pygame.display.set_mode((640, 480))  # Start the screen
+
+GREY = (50, 50, 50)
+LIGHT_GREY = (100, 100, 100)
+
+sprites = board_create()
+clicked_sprites = []
 
 running = True
 while running:
@@ -28,9 +42,10 @@ while running:
         if event.type == pygame.QUIT:  # The user closed the window!
             running = False  # Stop running
         if event.type == pygame.MOUSEBUTTONDOWN:
-            clicked_sprites = [s for s in sprites if s.rect.collidepoint(pygame.mouse.get_pos())]
+            clicked_sprites = [s for s in sprites if s.collidepoint(pygame.mouse.get_pos())]
+
     # Logic goes here
-    draw(3)
+    draw(clicked_sprites)
     pygame.display.flip()
 
 pygame.quit()  # Close the window
