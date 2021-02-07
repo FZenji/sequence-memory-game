fd07b9dbc874a5d1758554173f5d1bf81c794cdf
Laptop <henmtol@gmail.com>
2021-02-07 23:44:27 +0000

    Add controls info in pygame window, add break and play functionality

--- pi.py
+++ pi.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+pygame.font.init()
 
 
 def calc_pi(limit):  # Generator function
@@ -83,26 +85,57 @@ def draw():
 
 
 def show_pi(c_digit):
-    if pi[c_digit] == pi[c_digit-1]:
-        off += 70
-        print(off)
-        print((200+off) % 255, (70+off) % 255, (100+off) % 255)
-    else:
-        col_off = 0
-    pygame.draw.rect(screen, ((200+off) % 255, (70+off) % 255, (100+off) % 255), pad[pi[c_digit]])
+    global off
+    off = 0
+    i = c_digit
+    recurse = True
+    while recurse:
+        if pi[i] == pi[i-1]:
+            off += 1
+            i -= 1
+        else:
+            recurse = False
+    pygame.draw.rect(screen, ((200 + 200 * off) % 255,
+                              (70 + 70 * off) % 255,
+                              (100 + 100 * off) % 255), pad[pi[c_digit]])
+    digits_text = font_big.render("Digit: " + str(digit), False, (200, 30, 100))
+    break_text = font_big.render("Break: " + str(brk), False, (200, 30, 100))
+    hotkey_p = font_small.render("p = play", False, (200, 30, 100))
+    hotkey_b = font_small.render("b = set break", False, (200, 30, 100))
+    hotkey_e = font_small.render("e = end", False, (200, 30, 100))
+    hotkey_r = font_small.render("r = start", False, (200, 30, 100))
+    hotkey_a = font_small.render("a = quick back", False, (200, 30, 100))
+    hotkey_d = font_small.render("d = quick advance", False, (200, 30, 100))
+    hotkey_al = font_small.render("<- = back", False, (200, 30, 100))
+    hotkey_ar = font_small.render("-> = advance", False, (200, 30, 100))
+    screen.blit(digits_text, dest=((screen.get_width() // 2) - 73, 20))
+    screen.blit(break_text, dest=(20, 20))
+    screen.blit(hotkey_p, dest=(screen.get_width() - 180, 20))
+    screen.blit(hotkey_b, dest=(screen.get_width() - 180, 40))
+    screen.blit(hotkey_e, dest=(screen.get_width() - 180, 60))
+    screen.blit(hotkey_r, dest=(screen.get_width() - 180, 80))
+    screen.blit(hotkey_a, dest=(screen.get_width() - 180, 100))
+    screen.blit(hotkey_d, dest=(screen.get_width() - 180, 120))
+    screen.blit(hotkey_al, dest=(screen.get_width() - 180, 140))
+    screen.blit(hotkey_ar, dest=(screen.get_width() - 180, 160))
 
 
 screen = pygame.display.set_mode((640, 480))  # Start the screen
+clock = pygame.time.Clock()
+font_big = pygame.font.Font(pygame.font.get_default_font(), 36)
+font_small = pygame.font.Font(pygame.font.get_default_font(), 18)
+FPS = 30
 
 GREY = (50, 50, 50)
 LIGHT_GREY = (100, 100, 100)
 
-global off
 off = 0
 pad = board_create()
-nos_digits = 200
+nos_digits = 1000
 pi = pi_list(nos_digits)
-digit = 150
+digit = 762
+play = False
+brk = 0
 
 running = True
 while running:
@@ -116,12 +149,35 @@ while running:
             if event.key == pygame.K_LEFT:
                 if digit >= 1:
                     digit -= 1
+            if event.key == pygame.K_r:
+                digit = 0
+            if event.key == pygame.K_e:
+                digit = nos_digits
+            if event.key == pygame.K_p:
+                play = not play
+            if event.key == pygame.K_b:
+                brk = digit
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_d]:
+        if digit + 1 < len(pi):
+            digit += 1
+    if keys[pygame.K_a]:
+        if digit >= 1:
+            digit -= 1
+
+    if play:
+        if digit < brk:
+            time.sleep(1)
+            digit += 1
+        else:
+            play = not play
 
     # Logic goes here
-
     draw()
     show_pi(digit)
     pygame.display.flip()
+    screen.fill((0, 0, 0))
+    clock.tick(FPS)
 
 pi_print(nos_digits)
 pygame.quit()  # Close the window
